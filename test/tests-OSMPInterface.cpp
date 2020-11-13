44bdf2ae2f5558a257a97a631720bf2323fb195f
Nils Wendorff <Nils.Wendorff@dlr.de>
2020-11-13 11:49:06 +0100

    Add a test and test resources

---
+++ test/tests-OSMPInterface.cpp
@@ -0,0 +1,54 @@
+#include "catch2/catch.hpp"
+
+#include "OSMPInterface.h"
+#include "TestResourceDirectory.h"
+
+std::string osimessage = "";
+
+TEST_CASE("OSMP Interface Test") {
+
+	OSMPInterface source;
+
+	/*int create(std::string& path);
+	int init(float starttime = 0);
+	std::string read(std::string& name);
+	int write(std::string& name, std::string& value);
+	int doStep(double stepSize = 1);*/
+
+	CHECK(0 == source.create(testResourceDirectory + "/OSMPDummySource.fmu"));
+	CHECK(0 == source.init());
+
+	// OSMP-FMUs have to abide FMI, thus this variable defined as ausality="output" variability="discrete" initial="exact" has to be readable. It can return a nullpointer
+	std::string serializedSensorView = source.read("OSMPSensorViewOut");
+	CHECK(0 <= serializedSensorView.size());
+
+	osi3::SensorView sensorView;
+	sensorView.ParseFromString(serializedSensorView);
+
+	CHECK(serializedSensorView.size() == sensorView.ByteSizeLong());
+
+	CHECK(0 == source.doStep(0.06));
+
+	serializedSensorView = source.read("OSMPSensorViewOut");
+	CHECK(0 < serializedSensorView.size());
+
+	OSMPInterface sensor;
+
+	CHECK(0 == sensor.create(testResourceDirectory + "/OSMPDummySensor.fmu"));
+	CHECK(0 == sensor.init());
+
+	CHECK(0 == sensor.write("OSMPSensorViewIn", serializedSensorView));
+
+	CHECK(0 == sensor.doStep(0.06));
+
+	std::string serializedSensorData = sensor.read("OSMPSensorDataOut");
+	CHECK(0 < serializedSensorData.size());
+
+	osi3::SensorData sensorData;
+	sensorData.ParseFromString(serializedSensorData);
+
+	CHECK(0 < sensorData.ByteSizeLong());
+	CHECK(serializedSensorData.size() == sensorData.ByteSizeLong());
+
+}
+
