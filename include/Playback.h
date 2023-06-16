11d6ee57e3c68285d9681f5543b98a9e058deea2
bahn_bj <bjoern.bahn@dlr.de>
2023-06-16 08:49:18 +0200

    revert changes

--- include/Playback.h
+++ include/Playback.h
@@ -10,7 +10,6 @@
 #include <fstream>
 #include <sstream>
 #include <cmath>
-#include <queue>
 
 #include "ServiceInterface.h"
 #include "Utils.h"
@@ -30,7 +29,7 @@ private:
 	unsigned long long simulationTimeMicros;
 
 	std::vector<std::string> parseNextLine();
-	int addMovingObject(osi3::TrafficUpdate& trafficUpdate);
+	int createTrafficUpdateMessage(osi3::TrafficUpdate& trafficUpdate);
 	void createMovingObject(const std::vector<std::string>& values, osi3::MovingObject* movingObject);
 
 	uint8_t indexTS;
@@ -47,9 +46,6 @@ private:
 	uint8_t indexPositionX;
 	uint8_t indexPositionY;
 	uint8_t indexPositionZ;
-
-	std::map<uint64_t, osi3::TrafficUpdate> tempTrafficUpdates;
-	std::queue<std::pair<uint64_t, osi3::TrafficUpdate>> trafficUpdates;
 };
 
 #endif // !PLAYBACK_H
--- src/Playback.cpp
+++ src/Playback.cpp
@@ -57,24 +57,6 @@ void Playback::init(bool verbose, float starttime) {
 	currentLine = parseNextLine();
 
 	timeOffsetMicros = std::stoull(currentLine[indexTS]);
-	while (currentLine.size()) {
-		osi3::TrafficUpdate trafficUpdate;
-		uint64_t timestamp = std::stoull(currentLine[indexTS]);
-		if (tempTrafficUpdates.find(timestamp) == tempTrafficUpdates.end()) {
-			trafficUpdate = tempTrafficUpdates.at(timestamp);
-		}
-		addMovingObject(trafficUpdate);
-		tempTrafficUpdates.emplace(timestamp, trafficUpdate);
-		currentLine = parseNextLine();
-		if (verbose) {
-			std::cout << "Reach end of file." << std::endl;
-		}
-	}
-
-	for (auto& trafficUpdate : tempTrafficUpdates) {
-		trafficUpdates.push(trafficUpdate);
-	}
-	tempTrafficUpdates.clear();
 }
 
 std::vector<std::string> Playback::parseNextLine() {
@@ -98,12 +80,7 @@ int Playback::readOSIMessage(const std::string& name, std::string& message) {
 	int status = 1;
 	if (getMessageType(name) == eOSIMessage::TrafficUpdateMessage) {
 		osi3::TrafficUpdate trafficUpdate;
-		while (trafficUpdates.front().first - timeOffsetMicros <= simulationTimeMicros) {
-			trafficUpdate = trafficUpdates.front().second;
-			trafficUpdates.pop();
-		}
-		trafficUpdate.mutable_timestamp()->set_seconds((int64_t)simulationTimeMicros / 1000000);
-		trafficUpdate.mutable_timestamp()->set_nanos((uint32_t)((simulationTimeMicros % 1000000) * 1000));
+		status = createTrafficUpdateMessage(trafficUpdate);
 		trafficUpdate.SerializeToString(&message);
 		status = 0;
 	}
@@ -116,9 +93,21 @@ int Playback::doStep(double stepSize) {
 	return 0;
 }
 
-int Playback::addMovingObject(osi3::TrafficUpdate& trafficUpdate) {
-	osi3::MovingObject* movingObject = trafficUpdate.add_update();
-	createMovingObject(currentLine, movingObject);
+int Playback::createTrafficUpdateMessage(osi3::TrafficUpdate& trafficUpdate) {
+	while (std::stoull(currentLine[indexTS]) - timeOffsetMicros <= simulationTimeMicros) {
+		osi3::MovingObject* movingObject = trafficUpdate.add_update();
+		createMovingObject(currentLine, movingObject);
+		currentLine = parseNextLine();
+		if (currentLine.size() == 0) {
+			if (verbose) {
+				std::cout << "Reach end of file." << std::endl;
+			}
+			return 1;
+		}
+	}
+
+	trafficUpdate.mutable_timestamp()->set_seconds((int64_t)simulationTimeMicros / 1000000);
+	trafficUpdate.mutable_timestamp()->set_nanos((uint32_t)((simulationTimeMicros % 1000000) * 1000));
 	if (verbose) {
 		std::cout << "Traffic Update with " << trafficUpdate.update_size() << " vehicles." << std::endl;
 	}
