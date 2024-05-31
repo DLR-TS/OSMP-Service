593311ddbccaf7879c471899c3a20a54e15dea10
bahn_bj <bjoern.bahn@dlr.de>
2024-05-31 12:53:22 +0200

    fix compare function and update test

--- test/tests-main.cpp
+++ test/tests-main.cpp
@@ -4,3 +4,12 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
+#include "Utils.h"
+
+TEST_CASE("compare strings") {
+	std::string a = "OSMPSensorDATA[2]";
+	std::string b = "OSMPSensorDATA[1]";
+
+	bool c = matchingNames(a, b);
+	CHECK(!c);
+}
